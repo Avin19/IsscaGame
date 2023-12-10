07acd49d5d8dae02c761a495618660e09ded6782
Avin19 <avinashy1993@gmail.com>
2023-12-10 10:03:15 +0530

    Working on Level

---
+++ Assets/Scripts/Map Generation/Level.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Level 
+{
+   public static float _height =500;
+   public static float _width =500;
+
+
+
+   public static Sprite _treasureRoomIcon;
+   public static Sprite _bossRoomIcon;
+   public static Sprite _shopRoomIcon;
+
+}
