ed7dccfd003e8dd75d6d950b05528a291c55daba
Avin19 <avinashy1993@gmail.com>
2023-12-10 20:25:59 +0530

    Level Generator  No completed yet

---
+++ Assets/Scripts/Map Generation/GenerateLevel.cs
@@ -0,0 +1,109 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateLevel : MonoBehaviour
+{
+    [SerializeField] private Sprite _currentRoom;
+    [SerializeField] private Sprite _bossRoom;
+
+    [SerializeField] private Sprite _emptyRoom;
+
+    [SerializeField] private Sprite _shopRoom;
+    [SerializeField] private Sprite _treasureRoom;
+
+    [SerializeField] private Sprite _unexploredRoom;
+    void Start()
+    {
+        Level._defaultRoomIcon = _emptyRoom;
+        Level._bossRoomIcon = _bossRoom;
+        Level._treasureRoomIcon = _treasureRoom;
+        Level._currentRoomIcon = _currentRoom;
+        Level._shopRoomIcon = _shopRoom;
+        Level._unexploredIcon = _unexploredRoom;
+        //Drawing the start the first room
+        Room startRoom = new Room();
+        startRoom.location = new Vector2(0, 0);
+        startRoom.roomImage = Level._currentRoomIcon;
+
+        DrawRoomOnMap(startRoom);
+
+    }
+    private bool CheckIfRoomExist(Vector2 location)
+    {
+        return (Level.roooms.Exists(x => x.location == location));
+    }
+    int failsafe = 0;
+    private void GenerateRoom(Room roomR)
+    {
+
+        failsafe++;
+        DrawRoomOnMap(roomR);
+        if (failsafe > 50)
+        {
+
+            return;
+        }
+
+        if (Random.value > Level._roomGenerationChance)
+        {
+            Room room = new Room();
+            room.location = new Vector2(-1, 0) + room.location;
+            room.roomImage = Level._defaultRoomIcon;
+            if (!CheckIfRoomExist(room.location))
+            { GenerateRoom(room); }
+        }        //Right
+        if (Random.value > Level._roomGenerationChance)
+        {
+
+            Room room = new Room();
+            room.location = new Vector2(1, 0) + room.location;
+            room.roomImage = Level._defaultRoomIcon;
+            if (!CheckIfRoomExist(room.location))
+            { GenerateRoom(room); }
+        }
+        //Up
+        if (Random.value > Level._roomGenerationChance)
+        {
+            Room room = new Room();
+            room.location = new Vector2(0, 1) + room.location;
+            room.roomImage = Level._defaultRoomIcon;
+            if (!CheckIfRoomExist(room.location))
+            { GenerateRoom(room); }
+        }
+        //Down
+        if (Random.value > Level._roomGenerationChance)
+        {
+            Room room = new Room();
+            room.location = new Vector2(0, -1) + room.location;
+            room.roomImage = Level._defaultRoomIcon;
+            if (!CheckIfRoomExist(room.location))
+            { GenerateRoom(room); }
+        }
+    }
+
+    private void DrawRoomOnMap(Room r)
+    {
+        GameObject mapTile = new GameObject("Map Tile");
+        Image roomSprite = mapTile.AddComponent<Image>();
+        roomSprite.sprite = r.roomImage;
+        RectTransform roomRectTransfomr = mapTile.GetComponent<RectTransform>();
+        roomRectTransfomr.sizeDelta = new Vector2(Level._height, Level._width) * Level._scaleIcon;
+        roomRectTransfomr.position = r.location * (Level._scaleIcon * Level._height * Level._scaleMap + (Level._padding * Level._height * Level._scaleMap));
+        roomSprite.transform.SetParent(transform, false);
+        Level.roooms.Add(r);
+    }
+
+    private bool CheckIfRoomAroundGeneratedRoom(Vector2 v, string direction)
+    {
+        switch (direction)
+        {
+            case "Up":
+                { break; }
+        }
+        return false;
+    }
+}
--- Assets/Scripts/Map Generation/Level.cs
+++ Assets/Scripts/Map Generation/Level.cs
@@ -2,15 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Level 
+public static class Level
 {
-   public static float _height =500;
-   public static float _width =500;
+    public static float _height = 500;
+    public static float _width = 500;
+    public static float _padding =0.01f;
+    public static float _scaleMap = 1f;
+    public static float _scaleIcon = 0.1f;
+    public static float _roomGenerationChance = 0.5f;
+    public static Sprite _treasureRoomIcon;
+    public static Sprite _bossRoomIcon;
+    public static Sprite _shopRoomIcon;
+    public static Sprite _defaultRoomIcon;
+    public static Sprite _currentRoomIcon;
+    public static Sprite _unexploredIcon;
 
 
+    public static List<Room> roooms = new List<Room>();
 
-   public static Sprite _treasureRoomIcon;
-   public static Sprite _bossRoomIcon;
-   public static Sprite _shopRoomIcon;
+    public static Room _currentRoom;
+    
+
+}
+
+public class Room
+{
+    public int roomNumber =0;
+    public Vector2 location;
+
+    public Sprite roomImage;
+
 
 }
