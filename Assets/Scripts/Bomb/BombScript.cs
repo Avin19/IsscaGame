120a3196ed8a42910df9b55674c2dc6ed8ea1aa9
Avin19 <avinashy1993@gmail.com>
2024-06-02 14:54:11 +0530

    MainMenu

--- Assets/Scripts/Bomb/BombScript.cs
+++ Assets/Scripts/Bomb/BombScript.cs
@@ -6,22 +6,20 @@ public class BombScript : MonoBehaviour
 {
     private Transform bomb;
 
-    private void EndBomb()
+    public void EndBomb()
     {
         Player._status = "Idle";
         Player._playerStaff.SetActive(true);
     }
-    private void OnCollisionEnter(Collision other)
-    {
-        Debug.Log("" + other.gameObject.name);
-        if (other.gameObject.name != "StickWizardTutorial")
-        {
-            GameObject.Destroy(Instantiate(Player._pfBombExplosion, transform.position, Quaternion.identity), 3f);
-            EndBomb();
-            GameObject.Destroy(gameObject);
+    // private bool ableToDropBomb = false;
 
-        }
-    }
+    // private void WaitForBombDrop()
+    // {
+    //     ableToDropBomb = true;
+    // }
+
+
+
     private void Update()
     {
         if (Input.GetButtonUp("Bomb") && Player._status == "Idle")
@@ -30,12 +28,28 @@ public class BombScript : MonoBehaviour
             Player._playerStaff.SetActive(false);
             Player._animator.Play("HoldingBomb");
             bomb = Instantiate(Player._pfBomb, transform.position + new Vector3(0, 7, 0), Player._pfBomb.transform.rotation, transform).transform;
+            // Invoke(nameof(WaitForBombDrop), .1f);
         }
+        // if (Input.GetButtonUp("Bomb") && Player._status == "HoldingBomb" && ableToDropBomb)
+        // {
+        //     Player._status = "Idle";
+        //     Player._animator.Play("Idle");
+        //     ableToDropBomb = false;
+        //     Destroy(bomb.GetComponent<BombExplosion>());
+        //     bomb.transform.parent = null;
+        //     bomb.transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+        //     Rigidbody rb = bomb.GetComponent<Rigidbody>();
+        //     rb.constraints = RigidbodyConstraints.None;
+        //     rb.freezeRotation = true;
+        //     Invoke(nameof(EndBomb), .1f);
+
+        // }
         if (Input.GetButtonUp("Fire1") && Player._status == "HoldingBomb")
         {
             // Invoke(nameof(EndBomb), 1f);
             Player._status = "ThrowingBomb";
             Player._animator.Play("ThrowingBomb");
+            // ableToDropBomb = false;
             bomb.transform.parent = null;
             Rigidbody rb = bomb.GetComponent<Rigidbody>();
             rb.constraints = RigidbodyConstraints.None;
