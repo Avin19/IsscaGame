b9910943ff4785d5117233921c902232f3c8efe3
Avin19 <avinashy1993@gmail.com>
2024-06-04 09:15:00 +0530

    Working on Damage system

--- Assets/Scripts/Health/HealthManager.cs
+++ Assets/Scripts/Health/HealthManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 public class HealthManager : MonoBehaviour
@@ -7,7 +9,6 @@ public class HealthManager : MonoBehaviour
     [SerializeField] private Sprite halfHealth;
     [SerializeField] private Sprite emptyHealth;
 
-
     private void DrawHealth(Sprite Type)
     {
         GameObject health = new GameObject(Type.name);
@@ -16,9 +17,8 @@ public class HealthManager : MonoBehaviour
         image.sprite = Type;
 
     }
-    public void DrawPlayerHeath()
+    private void DrawPlayerHeath()
     {
-
         if (Player.health % 1 != 0)
         {
             for (int i = 0; i < Player.health - 1; i++)
@@ -35,7 +35,7 @@ public class HealthManager : MonoBehaviour
             }
 
         }
-        for (int i = 0; i < Player.maxHealth; i++)
+        for (int i = 0; i < Mathf.FloorToInt(Player.maxHealth - Player.health); i++)
         {
             DrawHealth(emptyHealth);
         }
@@ -45,6 +45,72 @@ public class HealthManager : MonoBehaviour
         DrawPlayerHeath();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log(Player.health);
+            TakeDamage(2);
+        }
+        if (Input.GetKey(KeyCode.H))
+        {
+            DrawPlayerHeath();
+        }
+    }
+
+
+    private void TakeDamage(int damage)
+    {
+        if (!Player.invincible)
+        {
+            Player.invincible = true;
+            CorountineManager.Instance.StartCoroutine(Uninvinvible());
+            Player.health -= damage;
+            if (Player.health <= 0)
+            {
+                CorountineManager.Instance.StartCoroutine(Die());
+            }
+            else
+            {
+                CorountineManager.Instance.StartCoroutine(WaitAndRedrawHearts());
+            }
+
+        }
+    }
+    public static IEnumerator Die()
+    {
+        Player._diePanel.SetActive(true);
+        yield return new WaitForSeconds(0);
+        foreach (RectTransform child in Player._healthPanel.transform.GetComponentsInChildren<RectTransform>())
+        {
+            Destroy(child.gameObject);
+        }
+        Player._animator.Play("Die");
+        Player.invincible = true;
+        Destroy(Player.transform.GetComponent<PlayerMovement>());
+        Destroy(Player.transform.GetComponent<PlayerAttack>());
+        Destroy(Player.transform.GetComponent<ChangeRoom>());
+        Destroy(Player.transform.GetComponent<BombScript>());
+        Player._diePanel.SetActive(false);
+
+    }
+    public IEnumerator WaitAndRedrawHearts()
+    {
+        Player._diePanel.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        Player._diePanel.SetActive(false);
+        yield return new WaitForSeconds(0.4f);
+        for (int i = 0; i < Player._healthPanel.transform.childCount; i++)
+        {
+            Destroy(Player._healthPanel.transform.GetChild(i).gameObject);
+        }
+        DrawPlayerHeath();
+    }
+    public static IEnumerator Uninvinvible()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Player.invincible = false;
+    }
 }
 
 
