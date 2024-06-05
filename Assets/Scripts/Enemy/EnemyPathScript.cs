d6cd2a666f8e9a9a24049e6e68fff1b864f716af
Avin19 <avinashy1993@gmail.com>
2024-06-05 10:59:30 +0530

    EnemyPath Script working

--- Assets/Scripts/Enemy/EnemyPathScript.cs
+++ Assets/Scripts/Enemy/EnemyPathScript.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
@@ -10,6 +11,7 @@ public class EnemyPathScript : MonoBehaviour
     [SerializeField] private List<Vector3> pathPoints = new List<Vector3>();
 
     private bool pathDrawn = false;
+    int numberpoint = 0;
 
     private void Update()
     {
@@ -19,11 +21,13 @@ public class EnemyPathScript : MonoBehaviour
             DrawPath();
         }
     }
-
-    private void DrawPath()
+    private void DrawPoint(Vector3 currentPoint)
     {
-
-        Vector3 dir = Player.transform.position - transform.position;
+        numberpoint++;
+        Debug.Log(numberpoint);
+        if (numberpoint >= 50) return;
+        Vector3 dir = Player.transform.position - currentPoint;
+        if (Vector3.Distance(Player.transform.position, currentPoint) < 5) return;
         bool checkDirection = true;
         int failsafe = -1;
         int counter = 0;
@@ -35,23 +39,32 @@ public class EnemyPathScript : MonoBehaviour
             if (failsafe > 15) return;
             int result = counter % 2 == 0 ? counter / 2 * 5 : -counter / 2 * 5;
             counter++;
-            Debug.Log(result);
+
             // Converting Vector 3 dir to rotation 
             Quaternion rotation = Quaternion.LookRotation(dir);
             // Adding the pendulum result to the rotation 
             rotation *= Quaternion.Euler(0f, result, 0f);
             //get back the vector3 from rotation 
             Vector3 adjustDir = rotation * Vector3.forward;
-            if (!Physics.Raycast(transform.position, adjustDir, out ray, distance))
+            if (!Physics.Raycast(currentPoint, adjustDir, out ray, distance))
             {
                 checkDirection = false;
-                pathPoints.Add(transform.position + dir.normalized * distance);
+                pathPoints.Add(currentPoint + dir.normalized * distance);
             }
-
-
+            else
+            {
+                if (ray.transform.name == "StickWizardTutorial")
+                {
+                    break;
+                }
+            }
         }
+        DrawPoint(pathPoints.Last());
 
-
+    }
+    private void DrawPath()
+    {
+        DrawPoint(transform.position);
     }
 }
 
