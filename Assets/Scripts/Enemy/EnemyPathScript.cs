3c2343cab1e6b9b425c75c05a797c7c51b2aada1
Avin19 <avinashy1993@gmail.com>
2024-06-05 11:50:49 +0530

    Implemented the Enemy Movement towards player

--- Assets/Scripts/Enemy/EnemyPathScript.cs
+++ Assets/Scripts/Enemy/EnemyPathScript.cs
@@ -1,38 +1,60 @@
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 
 
 public class EnemyPathScript : MonoBehaviour
 {
-    [SerializeField] private float distance = 5f;
+    [SerializeField] private float distance = 3f;
+    [SerializeField] private float repeatTimer = 2f;
 
-    [SerializeField] private List<Vector3> pathPoints = new List<Vector3>();
+    private List<Vector3> pathPoints = new List<Vector3>();
 
     private bool pathDrawn = false;
     int numberpoint = 0;
+    private Rigidbody rb;
+    private Vector3 moveTowardPlayer = Vector3.zero;
+    [SerializeField] private float speed = 2f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
         if (!pathDrawn)
         {
             pathDrawn = true;
-            DrawPath();
+            InvokeRepeating(nameof(DrawPath), 0, repeatTimer);
+        }
+        if (pathPoints.Count > 0)
+        {
+            moveTowardPlayer = pathPoints[0] - transform.position;
+            rb.velocity = moveTowardPlayer.normalized * speed;
+            if (Vector3.Distance(transform.position, pathPoints[0]) < 0.2f)
+            {
+                pathPoints.RemoveAt(0);
+            }
+            else
+            {
+                moveTowardPlayer = pathPoints[0] - transform.position;
+                rb.velocity = moveTowardPlayer.normalized * speed;
+            }
         }
     }
     private void DrawPoint(Vector3 currentPoint)
     {
-        numberpoint++;
-        Debug.Log(numberpoint);
-        if (numberpoint >= 50) return;
+
         Vector3 dir = Player.transform.position - currentPoint;
         if (Vector3.Distance(Player.transform.position, currentPoint) < 5) return;
         bool checkDirection = true;
         int failsafe = -1;
         int counter = 0;
         RaycastHit ray;
-        LayerMask layerMask = ~(1 << LayerMask.NameToLayer("Floor"));
+        LayerMask layerMask = ~(1 << LayerMask.NameToLayer("Enemy")) | (1 << LayerMask.NameToLayer("Floor"));
         while (checkDirection)
         {
             failsafe++;
@@ -46,7 +68,7 @@ public class EnemyPathScript : MonoBehaviour
             rotation *= Quaternion.Euler(0f, result, 0f);
             //get back the vector3 from rotation 
             Vector3 adjustDir = rotation * Vector3.forward;
-            if (!Physics.Raycast(currentPoint, adjustDir, out ray, distance))
+            if (!Physics.Raycast(currentPoint, adjustDir, out ray, distance, layerMask))
             {
                 checkDirection = false;
                 pathPoints.Add(currentPoint + dir.normalized * distance);
@@ -55,7 +77,9 @@ public class EnemyPathScript : MonoBehaviour
             {
                 if (ray.transform.name == "StickWizardTutorial")
                 {
+
                     break;
+
                 }
             }
         }
@@ -64,6 +88,7 @@ public class EnemyPathScript : MonoBehaviour
     }
     private void DrawPath()
     {
+        if (pathPoints.Count > 0) { pathPoints.Clear(); }
         DrawPoint(transform.position);
     }
 }
